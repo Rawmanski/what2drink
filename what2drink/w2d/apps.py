ca3489d074a0241c9904686a1aa7857a9adcb171
Roman Omarov <romo@tuta.io>
2023-02-02 00:16:31 +0100

    final project

---
+++ what2drink/w2d/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class W2DConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'w2d'
---
+++ what2drink/w2d/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name="index"),
+
+    path('search', views.search, name="search"),
+
+    path('about', views.about, name="about"),
+
+    path('contact', views.contact, name="contact")
+]
